defbedbfe9b5e09d10667ee5d91044b6e6212958
OrgesaBerisha <oorgesaberishaa@gmail.com>
2025-12-11 22:20:00 +0100

    fillimi i datacontex seed dhe program.js

--- StudentCourses/Data/DataContext.cs
+++ StudentCourses/Data/DataContext.cs
@@ -1,6 +1,21 @@
-﻿namespace StudentCourses.Data
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace StudentCourses.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)  //lidhjet bohen qetu one to many
+        {
+            // base.OnModelCreating(modelBuilder) 
+
+        }
+
     }
 }
